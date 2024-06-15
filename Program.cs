97f8c34985dc6798e40d500c1a85fd2df628c4e4
fidansuleymanva <tu5gllte6@code.edu.az>
2024-06-15 20:56:42 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Task1: Verilmiş ədəd 4 rəqəmli və 3 və ya 7 yə bölünürsə True əks halda False çap etsin
+using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a 4-digit number:");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 1000 && number <= 9999 && (number % 3 == 0 || number % 7 == 0))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
